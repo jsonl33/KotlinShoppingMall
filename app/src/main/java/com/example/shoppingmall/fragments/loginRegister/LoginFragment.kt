03995c53c9350331517ada26a95f9e6e10b72913
thisIsHyeongSeokLee <oddvapor@gmail.com>
2023-10-25 14:56:06 +0900

    프ë¡웰컴페이지 초안 작업

---
+++ app/src/main/java/com/example/shoppingmall/fragments/loginRegister/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.shoppingmall.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.shoppingmall.R
+
+class LoginFragment: Fragment(R.layout.fragment_login) {
+}
